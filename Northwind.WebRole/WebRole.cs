bb065536a727e8bd8134de0270d130af11a8ac77
mindencoded <illescas.gonzales@gmail.com>
2018-09-07 17:39:24 -0500

    Delete OnStart method from WebRole class

--- Northwind.WebRole/WebRole.cs
+++ Northwind.WebRole/WebRole.cs
@@ -5,13 +5,5 @@ namespace Northwind.WebRole
 {
     public class WebRole : RoleEntryPoint
     {
-        private static readonly TraceSource Trace = new TraceSource("Northwind.WebRole");
-        public override bool OnStart()
-        {
-            // Para obtener información sobre cómo administrar los cambios de configuración
-            // consulte el tema de MSDN en https://go.microsoft.com/fwlink/?LinkId=166357.
-            Trace.TraceInformation("WebRole is running.");
-            return base.OnStart();
-        }
     }
 }
